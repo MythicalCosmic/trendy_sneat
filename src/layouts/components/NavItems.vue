36a3c8e1be624b723e19d85503db2f063a581e14
Cosmic <qodirjonov0854@gmail.com>
2025-12-02 00:31:50 +0500

    Initial Commmit

---
+++ src/layouts/components/NavItems.vue
@@ -0,0 +1,31 @@
+<script setup>
+import VerticalNavSectionTitle from '@/@layouts/components/VerticalNavSectionTitle.vue'
+import VerticalNavGroup from '@layouts/components/VerticalNavGroup.vue'
+import VerticalNavLink from '@layouts/components/VerticalNavLink.vue'
+</script>
+
+<template>
+  <!-- 👉 Dashboards -->
+  <VerticalNavGroup
+    :item="{
+      title: 'Dashboards',
+      badgeContent: '5',
+      badgeClass: 'bg-error',
+      icon: 'bx-home-smile',
+    }"
+  >
+    <VerticalNavLink
+      :item="{
+        title: 'Analytics',
+        to: '/dashboard',
+      }"
+    />
+  </VerticalNavGroup>
+
+  <!-- 👉 Apps & Pages -->
+  <VerticalNavSectionTitle
+    :item="{
+      heading: 'Apps & Pages',
+    }"
+  />
+</template>
---
+++ src/plugins/router/routes.js
@@ -0,0 +1,27 @@
+export const routes = [
+  { path: '/', redirect: '/dashboard' },
+  {
+    path: '/',
+    component: () => import('@/layouts/default.vue'),
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/pages/dashboard.vue'),
+      },
+    ],
+  },
+  {
+    path: '/',
+    component: () => import('@/layouts/blank.vue'),
+    children: [
+      {
+        path: 'login',
+        component: () => import('@/pages/login.vue'),
+      },
+      {
+        path: '/:pathMatch(.*)*',
+        component: () => import('@/pages/[...error].vue'),
+      },
+    ],
+  },
+]
